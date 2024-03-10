907e0c97435fcc6c4a51e0106030e95f912c5eee
mertcebe <mr.mertcebe@gmail.com>
2024-03-10 14:22:05 +0300

    created some pages

--- src/app/sign-up/page.js
+++ src/app/sign-up/page.js
@@ -1,13 +1,15 @@
 "use client";
 
-import { auth } from '@/firebase/firebaseConfig';
-import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth, database } from '@/firebase/firebaseConfig';
+import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
+import { doc, setDoc } from 'firebase/firestore';
 import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/navigation';
 import React, { useState } from 'react'
 
-const SignInPage = () => {
+const SignUpPage = () => {
+    let [username, setUsername] = useState();
     let [email, setEmail] = useState();
     let [password, setPassword] = useState();
 
@@ -15,18 +17,36 @@ const SignInPage = () => {
 
     const submitFunc = async (e) => {
         e.preventDefault();
-        const {user: {displayName, email: emailInfo, photoURL, uid}} = await signInWithEmailAndPassword(auth, email, password);
-        router.push('/');
+        const { user } = await createUserWithEmailAndPassword(auth, email, password);
+        await updateProfile(user, {
+            displayName: username
+        });
+        await setDoc(doc(database, `users/${user.uid}`), {
+            displayName: username,
+            email: user.email,
+            photoURL: user.photoURL,
+            uid: user.uid
+        })
+        router.push('/sign-in');
     }
+
     return (
         <div className=' flex flex-col justify-center items-center w-full my-10'>
             <Image src={'/hippoicon.png'} width={100} height={100} alt='hippoicon' className='pointer-events-none' />
-            <h3 className='font-bold text-lg'>Sign in to your account</h3>
-            <Link href={`/sign-up`} className='text-xs mt-2 text-gray-400 hover:text-gray-700'>Don't have an account? &rarr;</Link>
+            <h3 className='font-bold text-lg'>Create your account</h3>
+            <Link href={`/sign-in`} className='text-xs mt-2 text-gray-400 hover:text-gray-700'>Already have an account? &rarr;</Link>
 
             <form class="bg-white rounded px-8 pt-6 pb-8 mb-4 w-96" onSubmit={submitFunc}>
                 <div class="mb-4">
                     <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+                        Username
+                    </label>
+                    <input onChange={(e) => {
+                        setUsername(e.target.value);
+                    }} class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="username" />
+                </div>
+                <div class="mb-4">
+                    <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
                         Email
                     </label>
                     <input onChange={(e) => {
@@ -41,14 +61,9 @@ const SignInPage = () => {
                         setPassword(e.target.value);
                     }} class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="password" />
                 </div>
-                <div className='text-end w-full mb-2'>
-                    <Link href={'/forgot-password'} class="inline-block align-baseline font-bold text-xs text-gray-400 hover:text-blue-300">
-                        Forgot Password?
-                    </Link>
-                </div>
                 <div class="flex items-center justify-between">
                     <button class="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
-                        Sign In
+                        Sign Up
                     </button>
                 </div>
             </form>
@@ -59,4 +74,4 @@ const SignInPage = () => {
     )
 }
 
-export default SignInPage
+export default SignUpPage
